665e1a467afc17f9d8c2709f27284565dd47b651
Taylor Thurman <taylor.thurman@protonmail.com>
2023-08-20 02:58:36 -0500

    Configured eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,49 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ['react-refresh'],
-  rules: {
+    "extends": [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier"
+],
+    "rules": {
     'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+        'warn',
+        {allowConstantExport: true},
     ],
-  },
+    "react/prop-types": 0,
+        "react/react-in-jsx-scope": 0,
+        "@typescript-eslint/no-empty-function": 0
+},
+    "plugins": ["react", "import", "jsx-a11y", "@typescript-eslint", 'react-refresh'],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+    "ecmaVersion": 2022,
+        "sourceType": "module",
+        "project": "tsconfig.json",
+        "ecmaFeatures": {
+        "jsx": true
+    }
+},
+    "env": {
+    "es6": true,
+        "browser": true,
+        "node": true
+},
+    "settings": {
+    "react": {
+        "version": "detect"
+    },
+    "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+        "typescript": {
+            "alwaysTryTypes": true
+        }
+    }
+}
 }
